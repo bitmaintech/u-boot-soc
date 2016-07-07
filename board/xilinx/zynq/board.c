eba3c5de831e94786899d82a8caa0c01e3c5c951
zhiqiang.liu <zhiqiang.liu@bitmain.com>
2016-07-07 11:31:29 +0800

    support upgrade done

--- board/xilinx/zynq/board.c
+++ board/xilinx/zynq/board.c
@@ -10,7 +10,12 @@
 #include <zynqpl.h>
 #include <asm/arch/hardware.h>
 #include <asm/arch/sys_proto.h>
-
+#include <linux/mtd/mtd.h>
+#include <malloc.h>
+#include <nand.h>
+#define UPGARDE_MARKER_ADDR     0x1040000
+#define UPGARDE_MARKER_LEN      16
+#define NAND_SIZE_LIMIT         (128*1024)
 DECLARE_GLOBAL_DATA_PTR;
 
 #if (defined(CONFIG_FPGA) && !defined(CONFIG_SPL_BUILD)) || \
@@ -78,6 +83,66 @@ int board_init(void)
 
 int board_late_init(void)
 {
+    /*Zynq7010 upgrade*/
+   nand_info_t *nand = NULL;
+   //nand = &nand_info[0];
+   unsigned int ret, i;
+   size_t read_len;
+   unsigned int marker_is_right = 0;
+   //char *nandroot = NULL;
+    char *bootargs = NULL;
+   unsigned char *upgrade_buf = NULL;
+
+   nand = &nand_info[0];
+   upgrade_buf = (unsigned char *)malloc(UPGARDE_MARKER_LEN*sizeof(unsigned char));
+   memset(upgrade_buf, 0, UPGARDE_MARKER_LEN);
+   read_len = UPGARDE_MARKER_LEN;
+   //ret = nand_read_skip_bad(nand, UPGARDE_MARKER_ADDR, &read_len, upgrade_buf);
+   ret = nand_read(nand, UPGARDE_MARKER_ADDR, &read_len, upgrade_buf);
+   //ret = nand_read_skip_bad(nand, UPGARDE_MARKER_ADDR, &read_len, NULL, NAND_SIZE_LIMIT, upgrade_buf);
+   if(!ret)
+   {
+       for(i=0; i<UPGARDE_MARKER_LEN; i++)
+       {
+           printk("--- upgrade marker upgrade_buf[%d] = 0x%x ---\n", i, *(upgrade_buf +i));
+       }
+
+       for(i=0; i<UPGARDE_MARKER_LEN; i++)
+       {
+           if(*(upgrade_buf + i) != i)
+           {
+               break;
+           }
+           marker_is_right = 1;
+       }
+
+       if(marker_is_right)
+       {
+           //setenv("nandroot", "/dev/mtdblock2");
+           //nandroot = getenv("nandroot");
+            setenv("bootargs", "noinitrd mem=1008M console=ttyPS0,115200 root=/dev/mtdblock2 rootfstype=jffs2 rw rootwait");
+            bootargs = getenv("bootargs");
+           //if(nandroot)
+            if(bootargs)
+           {
+               //printk("nandroot: %s\n", nandroot);
+                printk("bootargs: %s\n", bootargs);
+               printk("--- This time is boot for upgrade ---\n");
+           }
+           else
+           {
+               //printk("--- setenv nandroot to /dev/mtdblock2 failed! ---\n");
+                printk("--- setenv bootargs to /dev/mtdblock2 failed! ---\n");
+           }
+       }
+   }
+   else
+   {
+       printk("--- upgrade read nand error! ret = %d ---\n", ret);
+   }
+   free(upgrade_buf);
+   /*Zynq7010 upgrade end*/    
+
 	switch ((zynq_slcr_get_boot_mode()) & ZYNQ_BM_MASK) {
 	case ZYNQ_BM_QSPI:
 		setenv("modeboot", "qspiboot");
